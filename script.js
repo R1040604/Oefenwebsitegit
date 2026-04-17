d105caabb80ce26e4bf91ff1dd7f28f54f071f11
R1040604 <r1040604@student.thomasmore.be>
2026-04-17 10:36:47 +0200

    meer timer code

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 let timeLeft = 30;
 let gameTimer = null;
+
+
+function startGameTimer() {
+    clearInterval(gameTimer); // veiligheid
+
+    gameTimer = setInterval(() => {
+        timeLeft--;
+        updateTimerUI();
+
+        if (timeLeft <= 0) {
+            endGame();
+        }
+    }, 1000);
+}
+
+function updateTimerUI() {
+    document.getElementById("timer-indicator").innerText =
+        "Time: " + timeLeft;
+}
